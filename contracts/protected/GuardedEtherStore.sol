3c257b7b8b7d42363d3b89d9c435ed77cf4ff14f
kingsand <clinvil2@gmail.com>
2025-05-03 18:38:58 -0700

    Added the protected ether store contract as defense of re-entracy

--- contracts/protected/GuardedEtherStore.sol
+++ contracts/protected/GuardedEtherStore.sol
@@ -1,23 +1,28 @@
 pragma solidity ^0.8.29;
 
-contract EtherStore {
+contract SecureStore {
     mapping(address => uint256) public balances;
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
 
     function deposit() public payable {
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public noReentrant {
         uint256 bal = balances[msg.sender];
         require(bal > 0);
-
         (bool sent,) = msg.sender.call{value: bal}("");
         require(sent, "Failed to send Ether");
-
         balances[msg.sender] = 0;
     }
 
-    // Helper function to check the balance of this contract
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
